36f1da8b6138b1e120c2ada7967a1c239b94d5d9
Wilmer Nilsson <wilmernilsson@gmail.com>
2018-02-09 13:41:18 +0100

    röv

--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
+var x = [];
+var y = [];
+var v = 0;
 function setup() {
-  createCanvas(1400,700);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
 }
 
 function draw() {
-  i++;
-  background(i);
-  ellipse(mouseX, mouseY, mouseX, mouseY/2);
+  v += 0.07;
+  background(frameCount/2,255-frameCount/5,10);
+  rotate(v, [1,1,0]);
+  box();
+  stroke(255, 102, 0);
+  stroke(0, 0, 0);
+  if(x.length == 4){
+    bezier(x[0],y[0],x[1],y[1],x[2],y[2],x[3],y[3]);
+    x = [];
+    y = [];
+  }
+}
+
+function mousePressed(){
+  x.push(mouseX);
+  y.push(mouseY);
 }
