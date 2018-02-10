fb0a9323651c221831aa1b7644942ff1d1b6463e
Wilmer Nilsson <wilmernilsson@gmail.com>
2018-02-10 19:57:51 +0100

    adds surprise

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var yOff,xOff;
 var terrain = [];
 var flying;
 
+var nazi = -800;
+
 function setup() {
   scl = 30;
   yOff = 0;
@@ -21,12 +23,38 @@ function setup() {
 function draw() {
   stroke(255);
   background(0);
+  swastika(5,nazi);
   noFill();
   noStroke();
   rotateX(PI/3*(atan((mouseY-height/2)/(height/2))+10)/10);
   translate(-windowWidth*0.7,-windowHeight*0.7);
   flying -= 0.004;
   buildMesh(rows,cols,genTerr(flying,rows,cols),scl);
+}
+
+function swastika(s,z){
+  // beginShape(QUADS)
+  push();
+  translate(0,-200,z);
+  rotate(frameCount/100,[0,0,1]);
+  noStroke();
+  fill(20);
+  var l = 100;
+  box(l*s,l/5*s,l/5*s);
+  rotateZ(HALF_PI);
+  box(l*s,l/5*s,l/5*s);
+  translate(l*1/5*s,-l*2/5*s);
+  for(var i = 0; i < 4; i++){
+    box(l*(3/5)*s,l/5*s,l/5*s);
+    rotateZ(HALF_PI);
+    translate(l*(3/5)*s,-l/5*s);
+  }
+  translate(-l/5*s,2*l/5*s);
+  pop();
+}
+
+function mouseWheel(event) {
+  nazi += event.delta*3;
 }
 
 function genTerr(pos,row,col) {
@@ -42,6 +70,7 @@ function genTerr(pos,row,col) {
 }
 
 function buildMesh(row,col,terr,s){
+  push();
   for (var j = 0; j < row - 1; j++){
     beginShape(TRIANGLE_STRIP);
     for (var i = 0; i < col; i++){
@@ -51,4 +80,5 @@ function buildMesh(row,col,terr,s){
     }
     endShape();
   }
+  pop();
 }
