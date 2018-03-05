b0c3cdd692b278c20f90e008861626437edbb8f8
Wilmer Nilsson <wilmernilsson@gmail.com>
2018-03-05 08:20:42 +0100

    wishlist updated

--- start.js
+++ start.js
@@ -9,76 +9,77 @@ var frac;
 
 
 function setup() {
-  angleX = PI/3;
-  angleY = angleZ = yOff = yVel = xVel = 0;
-  highest = 200;
-  lowest = -200;
-  scl = 40;
-  v = 1/4;
-  a = 0.03;
-  createCanvas(windowWidth, windowHeight,WEBGL);
+    angleX = PI/3;
+    angleY = angleZ = yOff = yVel = xVel = 0;
+    highest = 200;
+    lowest = -200;
+    scl = 40;
+    v = 1/4;
+    a = 0.03;
 
-  cols = floor(windowWidth * 2 / scl);
-  rows = floor(windowWidth * 2 / scl);
+    createCanvas(windowWidth, windowHeight,WEBGL);
 
-  terrain = [];
-  for(var i = 0; i < cols; i++){
-    terrain[i] = [];
-    for(var j = 0; j < cols; j++){
-      terrain[i][j] = 0;
+    cols = floor(windowWidth * 2 / scl);
+    rows = floor(windowWidth * 2 / scl);
+
+    terrain = [];
+    for(var i = 0; i < cols; i++){
+        terrain[i] = [];
+        for(var j = 0; j < cols; j++){
+            terrain[i][j] = 0;
+        }
     }
-  }
 }
 
 function draw() {
-  background(0);
-  noStroke();
+    background(0);
+    noStroke();
 
-  angleX = atan(mouseY/height-3/5);
-  angleY = atan(1/2-mouseX/width)/(abs(a*1.3)+1);
-  angleZ = angleZ + angleY*v/10;
+    angleX = atan(mouseY/height-3/5);
+    angleY = atan(1/2-mouseX/width)/(abs(a*1.3)+1);
+    angleZ = angleZ + angleY*v/10;
 
-  rotateX(PI/4*angleX+PI/3);
-  rotateY(PI/4*angleY);
-  rotateZ(angleZ);
+    rotateX(PI/4*angleX+PI/3);
+    rotateY(PI/4*angleY);
+    rotateZ(angleZ);
 
-  translate(0,0,-angleX*100);
+    translate(0,0,-angleX*100);
 
-  a = constrain(a-(angleX+v/3)*v/50, -v/10, v);
+    a = constrain(a-(angleX+v/3)*v/50, -v/10, v);
 
-  yVel -= cos(angleZ)*a;
-  xVel -= sin(angleZ)*a;
+    yVel -= cos(angleZ)*a;
+    xVel -= sin(angleZ)*a;
 
-  buildMesh(rows,cols,genTerrain(xVel,yVel,rows,cols),scl);
+    buildMesh(rows,cols,genTerrain(xVel,yVel,rows,cols),scl);
 }
 
 
 function genTerrain(posX,posY,rows,cols) {
-  for(var j = 0; j < rows; j++){
-    xOff = posX;
-    for(var i = 0; i < cols; i++){
-      terrain[i][j] = map(noise(xOff,posY), 0, 1, lowest, highest);
-      xOff += 0.2;
+    for(var j = 0; j < rows; j++){
+        xOff = posX;
+        for(var i = 0; i < cols; i++){
+            terrain[i][j] = map(noise(xOff,posY), 0, 1, lowest, highest);
+            xOff += 0.2;
+        }
+        posY += 0.2;
     }
-    posY += 0.2;
-  }
-  return terrain;
+    return terrain;
 }
 
 
 function buildMesh(rows,cols,terr,scl){
-  push();
-  translate(-windowWidth,-windowWidth);
-  for (var j = 0; j < rows - 1; j++){
-    beginShape(TRIANGLE_STRIP);
-    for (var i = 0; i < cols; i++){
-      fill(map(terr[i][j],lowest,highest,0,10)
-            ,map(terr[i][j],lowest,highest,0,10)
-            ,map(terr[i][j],lowest,highest,0,200));
-      vertex(i*scl, j*scl, terr[i][j]);
-      vertex(i*scl, (j+1)*scl, terr[i][j+1]);
+    push();
+    translate(-windowWidth,-windowWidth);
+    for (var j = 0; j < rows - 1; j++){
+        beginShape(TRIANGLE_STRIP);
+        for (var i = 0; i < cols; i++){
+            fill(map(terr[i][j],lowest,highest,0,10)
+                ,map(terr[i][j],lowest,highest,0,10)
+                ,map(terr[i][j],lowest,highest,0,200));
+            vertex(i*scl, j*scl, terr[i][j]);
+            vertex(i*scl, (j+1)*scl, terr[i][j+1]);
+        }
+        endShape();
     }
-    endShape();
-  }
-  pop();
+    pop();
 }
