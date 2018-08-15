4d2a9c68b7dc93714b5a81cfcdcdfc17e0f9bef5
Wilmer Nilsson <wilmernilsson@gmail.com>
2018-08-15 13:13:39 +0200

    signalbehandling

--- .pandocfixer.sh
+++ .pandocfixer.sh
@@ -2,4 +2,7 @@
 
 title=$(echo "$1" | sed "s/[^a-zA-Z]*//")
 
-pandoc -s _drafts/$1.md -o _posts/temp$1.html --mathjax && echo -e "---\nlayout: post\ntitle: \"$title\"\n---" > _posts/$1.html && cat _posts/temp$1.html >> _posts/$1.html && rm _posts/temp$1.html
+pandoc -s _drafts/$1.md -o _posts/temp$1.html --mathjax && 
+  echo -e "---\nlayout: post\ntitle: \"$title\"\n---" > _posts/$1.html && 
+  cat _posts/temp$1.html >> _posts/$1.html && 
+  rm _posts/temp$1.html
