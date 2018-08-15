33bc24d96c49fef966e8ecb63a7ae5debe6137bd
Wilmer Nilsson <wilmernilsson@gmail.com>
2018-08-15 15:00:52 +0200

    will it work?

--- .pandocfixer.sh
+++ .pandocfixer.sh
@@ -2,7 +2,7 @@
 # Run this together with the name of the _draft/-file that you want --mathjax to fix math for
 # $1 = filename without extention
 
-title=$(echo "$1" | sed "s/[^a-zA-Z]*//")
+title=$(echo "$1" | sed -e "s/[^a-zA-Z]*//" -e "s/_/ /g")
 
 pandoc -s _drafts/$1.md -o _posts/temp$1.html --mathjax && 
   echo -e "---\nlayout: post\ntitle: \"$title\"\n---" > _posts/$1.html && 
